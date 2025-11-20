179b5081d23a7f1eea0dfd3a8d27ffc39f97bc4b
Julien Baouz <baouzjulien@gmail.com>
2025-11-20 21:25:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// définition des variables
+// définition des variables principales
 const formulaire = document.getElementById('form');
 const personalInfo = document.getElementById('personal-info');
 const productInfo = document.getElementById('product-info');
@@ -7,3 +7,18 @@ const complaintDescription = document.getElementById('complaint-description-cont
 const desiredSolution = document.getElementById('solutions-group');
 const solutionDescription = document.getElementById('solution-description-container');
 const submitBtn = document.getElementById('submit-btn');
+
+// variables spécifiques
+const isEmptyFullName = formulaire.elements['full-name'].value.trim()  === "";
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const isEmailValid = emailRegex.test(formulaire.elements['email'].value);
+
+// Fonction de vérification des input
+function checkErrors(e) {
+  e.preventDefault();
+
+}
+
+
+// Event listener sur submit btn
+submitBtn.addEventListener('submit', checkErrors);
