c5ad3d4da09c8ba746aadb04d6327f2c5a7ef411
Julien Baouz <baouzjulien@gmail.com>
2025-11-21 18:07:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,16 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const orderNumRegex = /^2024\d{10}$/;
 const productCodeRegex = /^[A-Z]{2}\d{2}-[A-Z]\d{3}-[A-Z]{2}\d$/i;
 
-
 // variables spécifiques de filtre
 const isEmptyFullName = formulaire.elements['full-name'].value.trim()  === "";
 const isEmailValid = emailRegex.test(formulaire.elements['email'].value);
 const isValidOrderNum = orderNumRegex.test(formulaire.elements['order-no'].value);
-const isValidProductCode = productCodeRegex.test(formulaire.elements['product-code'].value)
+const isValidProductCode = productCodeRegex.test(formulaire.elements['product-code'].value);
+const isPositiveQuantity = Number(formulaire.elements['quantity'].value) >= 1;
+const isAtLeastOneChecked = complaintGroup.querySelectorAll('input[type="checkbox"]:checked').length > 0;
+const iscomplaintDescription = complaintDescription.value.length > 19 && complaintGroup.querySelector('#other-complaint:checked') !== null;
+const isSelectedSolution = desiredSolution.querySelectorAll('input[type="radio"]:checked').length > 0;
+const isSolutionDescription = solutionDescription.value.length > 19 && desiredSolution.querySelector('#other-solution:checked') !== null;
 
 // Fonction de vérification des input
 function checkErrors(e) {
